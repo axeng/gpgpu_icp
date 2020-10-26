196539b504cfb1c085992fcd2ae9b89750837509
Romain Schloesing <romain.schloesing@epita.fr>
2020-10-26 14:21:26 +0100

    transform: Deletion of useless transpose function

--- src/cpu/transform/transform.cc
+++ src/cpu/transform/transform.cc
@@ -6,6 +6,50 @@ namespace transform
     // Input : array containing tuple of 3 elements
     unsigned int getFitTransform(std::vector<std::tuple<double, double, double>>& first, std::vector<std::tuple<double, double, double>>& second)
     {
+/*
+def best_fit_transform(A, B):
+    '''
+    Calculates the least-squares best-fit transform that maps corresponding points A to B in m spatial dimensions
+    Input:
+      A: Nxm numpy array of corresponding points
+      B: Nxm numpy array of corresponding points
+    Returns:
+      T: (m+1)x(m+1) homogeneous transformation matrix that maps A on to B
+      R: mxm rotation matrix
+      t: mx1 translation vector
+    '''
+
+    assert A.shape == B.shape
+
+    # get number of dimensions
+    m = A.shape[1]
+
+    # translate points to their centroids
+    centroid_A = np.mean(A, axis=0)
+    centroid_B = np.mean(B, axis=0)
+    AA = A - centroid_A
+    BB = B - centroid_B
+
+    # rotation matrix
+    H = np.dot(AA.T, BB)
+    U, S, Vt = np.linalg.svd(H)
+    R = np.dot(Vt.T, U.T)
+
+    # special reflection case
+    if np.linalg.det(R) < 0:
+       Vt[m-1,:] *= -1
+       R = np.dot(Vt.T, U.T)
+
+    # translation
+    t = centroid_B.T - np.dot(R,centroid_A.T)
+
+    # homogeneous transformation
+    T = np.identity(m+1)
+    T[:m, :m] = R
+    T[:m, m] = t
+
+    return T, R, t*/
+
         unsigned int result = 0;
 
         // Check shape for each set
@@ -201,24 +245,6 @@ namespace transform
                 }
             }
         }
-    }
-
-    /**
-     * Return the transpose of the given set
-     */
-    void transpose(const std::vector<std::vector<double>>& firstSet,
-        std::vector<std::vector<double>>& result)
-    {
-        result = std::vector()
-
-        for(std::vector<double>::size_type i = 0; i < firstSet[0].size(); i++)
-        {
-            for (std::vector<int>::size_type j = 0; j < firstSet.size(); j++)
-            {
-                result[i][j] = firstSet[j][i];
-            }
-        }
-        return result;
     }
 
     /**

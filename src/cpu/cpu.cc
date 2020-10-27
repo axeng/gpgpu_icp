d4d877d3c161c2d984fb7e0f3b5afbca79451680
Axen Georget <mail@axen.io>
2020-10-27 18:07:03 +0100

    fix: cpu.cc points_t old types

--- src/cpu/cpu.cc
+++ src/cpu/cpu.cc
@@ -11,23 +11,23 @@ int main(int argc, char* argv[])
         return 1;
     }
     */
-    parser::points_t vect_point;
+    parser::matrix_t vect_point;
     // bool ret = parser::parse_file(argv[1], vect_point);
 
-    parser::point_t Aone = {0, 0, 0};
-    parser::point_t Atwo = {0.5, 0.5, 0.5};
-    parser::point_t Athree = {1, 1, 1};
+    parser::vector_t Aone({0, 0, 0});
+    parser::vector_t Atwo({0.5, 0.5, 0.5});
+    parser::vector_t Athree({1, 1, 1});
 
-    parser::points_t A;
+    parser::matrix_t A;
     A.push_back(Aone);
     A.push_back(Atwo);
     A.push_back(Athree);
 
-    parser::point_t Bone = {0.5, 0.5, 0.5};
-    parser::point_t Btwo = {1, 1, 1};
-    parser::point_t Bthree = {1.5, 1.5, 1.5};
+    parser::vector_t Bone({0.5, 0.5, 0.5});
+    parser::vector_t Btwo({1, 1, 1});
+    parser::vector_t Bthree({1.5, 1.5, 1.5});
 
-    parser::points_t B;
+    parser::matrix_t B;
     B.push_back(Bone);
     B.push_back(Btwo);
     B.push_back(Bthree);

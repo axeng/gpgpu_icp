ba346ccbe55d58e6aa4b400cef3d8ce3086937cf
Clement Reau <clement.reau@epita.fr>
2020-10-31 10:22:03 +0100

    gpu_test: fix wrong using

--- src/gpu_test/parser/parser.hh
+++ src/gpu_test/parser/parser.hh
@@ -6,12 +6,13 @@
 #include <string>
 #include <tuple>
 #include <vector>
+#include "gpu_test/utils/utils.hh"
 
 namespace parser
 {
-    using value_t = utils::Matrix::value_t;
-    using vector_t = utils::Matrix::vector_t;
-    using matrix_t = utils::Matrix::matrix_t;
+    using value_t = utils::value_t;
+    using vector_t = utils::vector_t;
+    using matrix_t = utils::matrix_t;
 
     bool parse_file(const std::string& path, matrix_t& point_list);
 } // namespace parser
